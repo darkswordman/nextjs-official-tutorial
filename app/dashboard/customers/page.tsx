c309336cd27add8db840e98567a55812073e738a
crisdev <cristian.sprohnle@gmail.com>
2023-11-06 22:21:17 -0300

    feat: app: first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export const CustomersPage = (): JSX.Element => {
+  return <p>Customers Page</p>;
+};
+
+export default CustomersPage;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export const InvoicesPage = (): JSX.Element => {
+  return <p>Invoices page</p>;
+};
+
+export default InvoicesPage;
